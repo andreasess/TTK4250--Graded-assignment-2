b3443d464026d899df4088a68f976a952c395d23
andreasess <anstei@stud.ntnu.no>
2021-10-19 14:30:47 +0200

    First commit

---
+++ eskf/cross_matrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+from numpy import ndarray
+
+import solution
+
+
+def get_cross_matrix(vec: ndarray) -> ndarray:
+    """Get the matrix equivalent of cross product. S() in (10.68)
+
+    cross_product_matrix(vec1)@vec2 == np.cross(vec1, vec2)
+
+    Hint: see (10.5)
+
+    Args:
+        vec (ndarray[3]): vector
+
+    Returns:
+        S (ndarray[3,3]): cross product matrix equivalent
+    """
+
+    # TODO replace this with your own code
+    #S = solution.cross_matrix.get_cross_matrix(vec)
+    S = np.ndarray((3, 3))
+    S = [[0, -vec[2], vec[1]],
+        [vec[2], 0, -vec[0]],
+        [-vec[1], vec[0], 0]]
+
+    return S
